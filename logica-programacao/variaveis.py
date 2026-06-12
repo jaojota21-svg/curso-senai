f812e4a494b19d89ea07df26d8b8f9864fbf3a86
christopher <jaojota21@gmail.com>
2026-06-12 15:01:07 -0300

    mensagem

---
+++ logica-programacao/variaveis.py
@@ -0,0 +1,13 @@
+nome = input("digite seu nome")
+sobrenome = input("digite seu sobrenome")
+
+idade = int(input("digite sua idade"))
+numero = int(input("digite seu numero"))
+
+salario = float(input("digite seu salario"))
+altura = float(input("digite sua altura"))
+
+mundial = input("gosta de mundial s/n: ")
+liverpool = input("gosta do liverpool s/n: ")
+
+print(nome, sobrenome, idade, numero, salario, altura, mundial, liverpool)
